56ed0fd81f2920a86c49d1cf4bedf3ee020fd638
aliemirozen <aliemirozen@gmail.com>
2023-01-23 17:38:14 +0300

    upgrade

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,25 +12,6 @@
 #include "libft.h"
 #include "stdio.h"
 
-void    *ft_memcpy(void *dst, const void *src, size_t n)
-{
-    char    *str;
-    char    *src2;
-    size_t    i;
-
-    i = 0;
-    str = (char *)dst;
-    src2 = (char *)src;
-    if (dst == NULL && src == NULL)
-        return (NULL);
-    while (i < n)
-    {
-        str[i] = src2[i];
-        i++;
-    }
-    return (dst);
-}
-
 void    *ft_memmove(void *dst, const void *src, size_t len)
 {
     char    *dest;
